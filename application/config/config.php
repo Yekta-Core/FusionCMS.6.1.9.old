82c21228e3509f2215b6243d67935545efb81fee
Nightprince <keramatjokar8855@gmail.com>
2018-02-23 21:49:55 +0330

    Delete additional line

--- application/config/config.php
+++ application/config/config.php
@@ -183,7 +183,6 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 | use segment based URLs.
 |
 */ 
-$config['allow_get_array']		= TRUE;
 $config['enable_query_strings'] = FALSE;
 $config['controller_trigger'] = 'c';
 $config['function_trigger'] = 'm';
