f39f1c7b7b5a3b5f06eabd205361da123e220b76
Nightprince <keramatjokar8855@gmail.com>
2018-02-25 23:05:50 +0330

    Fix wod and legion in ucp

--- application/language/english/ucp.php
+++ application/language/english/ucp.php
@@ -61,3 +61,5 @@ $lang['tbc'] = "The Burning Crusade";
 $lang['wotlk'] = "Wrath of The Lich King";
 $lang['cata'] = "Cataclysm";
 $lang['mop'] = "Mists of Pandaria";
+$lang['wod'] = "Warlords of Draenor";
+$lang['legion'] = "Legion";
